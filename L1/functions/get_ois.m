b99b15a1fc2981b39d42c336fa82de026e704c3f
AdamHallberg <146318863+AdamHallberg@users.noreply.github.com>
2025-09-15 16:26:48 +0200

    Update get_ois.m

--- L1/functions/get_ois.m
+++ L1/functions/get_ois.m
@@ -1,16 +1,20 @@
-function OISs = get_ois(filename)
-
+function [OISs, option_data] = real_excel_file(filename)
+    ois_data = readtable(filename);
+    d = ois_data(2:end, [5 6]);
     % Read maturities (col D) and mid rates (col F) from Excel
-    maturities = readmatrix(filename, 'Range', 'D3:D24');
-    mids       = readmatrix(filename, 'Range', 'F3:F24');
+    maturities = floor(d(:,"Var5")-d(1,"Var5"))./365; % [years]
+    mids       = d(:,2).*0.01;
+    n = max(size(maturities));
 
-    % Preallocate struct array
-    n = length(maturities);
-    OISs = struct('Maturity', cell(1, n), 'Mid', cell(1, n));
+    OISs = [maturities, mids];
 
-    % Fill struct array
-    for i = 1:n
-        OISs(i).Maturity = maturities(i);
-        OISs(i).Mid      = mids(i);
-    end
+
+    option_data = readtable("ois_data.xlsx", "Sheet", "filtered_option_data");
+    option_data = table2array(option_data);
+
+    % 4 is "arbitrary"
+    option_data(:, 3) = option_data(:, 3) - option_data(1, 3) + 4; % get time diff
+    option_data(:, 3) = option_data(:, 3) / 365; % conver to fraction of years
+    
+    
 end

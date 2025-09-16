64df4c31172212a20c950a25a32d69b70cba4102
Adam <ah.adamhallberg@gmail.com>
2025-09-16 12:35:43 +0200

    begining of task 2

--- L1/functions/andersen_ratcliffe_coeff.m
+++ L1/functions/andersen_ratcliffe_coeff.m
@@ -1,27 +1,26 @@
-function [A, B, alpha_1, gamma_end] = andersen_ratcliffe_coeff(S_j, b_hat, v_hat, dt, dS)
+function [A, B, alpha_1, gamma_end] = andersen_ratcliffe_coeff(S_j, r, sigma, i, dt, dS)
     % Create Matrices and Coefficients for the Crank-Nicolson scheme (theta = 0.5)
     %
     % Correct form: A*F^{n} = B*F^{n+1}
+    v_hat = sigma(i)^2;       
+    b_hat = r(i) - 0.5*v_hat;  % N long
 
     M = length(S_j);
 
-    alpha = dt/(dS^2);
-
     % Coefficients for L without dt
-    u = 0.5 * alpha * (v_hat + dS*b_hat);
-    c = - alpha * (v_hat);
-    l = 0.5 * alpha * (v_hat - dS*b_hat);
+    a = (v_hat) / (2 * dS^2) - b_hat / (2 * dS);
+    b = - (v_hat) / (dS^2) - r;
+    c = (v_hat) / (2 * dS^2) + b_hat / (2 * dS);
     
     % Build L_mat (tridiagonal matrix for L)
-    L_mat = diag(c) + diag(u(2:end), -1) + diag(l(1:end-1), 1);
+    L_mat = diag(b) + diag(a(2:end), -1) + diag(c(1:end-1), 1);
     
     % Crank-Nicolson matrices
     A = eye(M) - 0.5 * dt * L_mat;
     B = eye(M) + 0.5 * dt * L_mat;
 
-    % Coeficcients needed for handeling the boundary conditions. By looking
+    % Coeficcients needed for handeling the boundary conditions. By ooking
     % at the matrices in a) it is obvious where these come from. 
-    alpha_1 = (sigma^2 * S_j(1)^2) / (2 * dS^2) - ((r-q) * S_j(1)) / (2 * dS);
-    gamma_end = (sigma^2 * S_j(end)^2) / (2 * dS^2) + ((r-q) * S_j(end)) / (2 * dS);
-    
+    alpha_1 = a;
+    gamma_end = c;
 end

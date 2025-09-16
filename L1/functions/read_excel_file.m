a611fe200d58b8540dd50c2fb6c55382017defe2
Adam <ah.adamhallberg@gmail.com>
2025-09-16 12:17:20 +0200

    rename

---
+++ L1/functions/read_excel_file.m
@@ -0,0 +1,20 @@
+function [OISs, option_data] = read_excel_file(filename)
+    ois_data = readtable(filename);
+    d = ois_data(2:end, [5 6]);
+    % Read maturities (col D) and mid rates (col F) from Excel
+    maturities = floor(d(:,"Var5")-d(1,"Var5"))./365; % [years]
+    mids       = d(:,2).*0.01;
+    n = max(size(maturities));
+
+    OISs = [maturities, mids];
+
+
+    option_data = readtable("ois_data.xlsx", "Sheet", "filtered_option_data");
+    option_data = table2array(option_data);
+
+    % 4 is "arbitrary"
+    option_data(:, 3) = option_data(:, 3) - option_data(1, 3) + 4; % get time diff
+    option_data(:, 3) = option_data(:, 3) / 365; % conver to fraction of years
+    
+    
+end
